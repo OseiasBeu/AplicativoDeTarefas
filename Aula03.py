f70f597836a0e169b541f354fb273648662b2dee
Oseias Beu <oseiasbeu@outlook.com>
2022-01-04 16:17:48 -0300

    Eventos e variaveis de instancia

---
+++ Aula03.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+
+class Teste(App):
+    def build(self):
+        box = BoxLayout(orientation='vertical')
+        button = Button(text='Botão 1',font_size='30',on_release=self.incrementar)
+        self.label = Label(text='0',font_size=30)
+        box.add_widget(button)
+        box.add_widget(self.label)
+        return box
+    
+    def incrementar(self,button):
+        self.label.text = str(int(self.label.text) +1)
+        
+
+Teste().run()
+
